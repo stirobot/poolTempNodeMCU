5595924464f73df932e067c601dbbfb9dc920202
stirobot <Justin.nev@gmail.com>
2018-02-25 10:30:36 -0800

    init

---
+++ poolTempNodeMCU.ino
@@ -0,0 +1,35 @@
+//gets pool temp from DS18B20 sensor and posts it to thinger.io
+
+#include <ESP8266WiFi.h>
+#include <ThingerESP8266.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
+#define ONE_WIRE_BUS D2     // what pin we're connected to
+OneWire oneWire(ONE_WIRE_BUS);
+
+DallasTemperature sensors(&oneWire);
+
+// Configuration of the WiFi AP settings.
+#define WIFI_AP "apname"
+#define WIFI_PASSWORD "apassword"
+
+
+ThingerESP8266 thing("a", "a", "a");
+
+void setup() {
+  Serial.begin(9600);
+  sensors.begin();
+  thing.add_wifi(WIFI_AP, WIFI_PASSWORD);
+  thing["poolTemp"] >> outputValue(sensors.getTempFByIndex(0));
+  Serial.begin(9600);
+}
+
+void loop() {
+  //read data
+  thing.handle();
+  sensors.requestTemperatures();
+  Serial.println(sensors.getTempFByIndex(0));
+}
+
